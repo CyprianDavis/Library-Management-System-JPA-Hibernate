008283cf3f146e10e93bf4b19f2c6bb1c21261c3
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-08-22 22:32:10 +0300

    added MemberOpsTest and tested idGeneration for member

--- src/test/java/MemberOpsTest.java
+++ src/test/java/MemberOpsTest.java
@@ -1,4 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import data.model.member.Member;
+import database.memberOperations.MembersOperations;
 
 public class MemberOpsTest {
+	List<Member>members = new LinkedList<>();
+	@Test
+	void testgetNextId() {
+		int id = MembersOperations.getNextTableGeneratorValue();
+		Assertions.assertEquals(2, id);
+	}
+	
 
 }
