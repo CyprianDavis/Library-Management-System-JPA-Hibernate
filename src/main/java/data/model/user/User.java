d116259a372958d495ff136138aada49c65a4c70
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-08-23 12:52:14 +0300

    added named Queries on User.class

--- src/main/java/data/model/user/User.java
+++ src/main/java/data/model/user/User.java
@@ -2,10 +2,18 @@ package data.model.user;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="users")
+@NamedQueries({
+	@NamedQuery(name="Users.findAllUsers",query="SELECT u FROM Users u"),
+	@NamedQuery(name="Users.login", query="SELECT u FROM Users u WHERE u.userName=:name AND u.passWord=:password"),
+	@NamedQuery(name="Users.findUserByName",query="SELECT u FROM Users u WHERE u.userName=:name")
+	
+})
 public class User {
 	@Id
 	private String userName;
