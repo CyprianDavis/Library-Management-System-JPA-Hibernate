3d1322e40771ec513cd8ee8ebf38e8fa3306463b
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-08-30 15:48:53 +0300

    fixed MemberDetailsController

--- src/main/java/ui/viewMemberDetails/MemberDetailsController.java
+++ src/main/java/ui/viewMemberDetails/MemberDetailsController.java
@@ -3,12 +3,15 @@ package ui.viewMemberDetails;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import data.model.member.Member;
+import database.memberOperations.MembersOperations;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
 public class MemberDetailsController implements Initializable{
@@ -43,11 +46,19 @@ public class MemberDetailsController implements Initializable{
 	public static void setIdNum(String idNum) {
 		id =idNum;
 		
-		
-	
 	}
 	
 	private  void loadMemberDetails() {
+		Member member = MembersOperations.findMember(id);//extracts member object
+		idNum.setText(member.getMemberId());
+		name.setText(member.getSurName()+" "+member.getGivenName()+" "+member.getOtherName());
+		sex.setText(member.getGender());
+		contact.setText(member.getContact());
+		email.setText(member.getEmail());
+		address.setText(member.getAddress());
+		img = member.getImage();
+		image.setFill(new ImagePattern(img));
+		dateOfReg.setText(member.getDateOfReg());
 		
 		}
 	}
