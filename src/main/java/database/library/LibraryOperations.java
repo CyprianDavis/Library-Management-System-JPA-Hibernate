39186260c86803f47fd71237fb7369809d39f5c2
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-08-30 16:20:52 +0300

    added methods to LibraryOperations.class

--- src/main/java/database/library/LibraryOperations.java
+++ src/main/java/database/library/LibraryOperations.java
@@ -3,6 +3,8 @@ package database.library;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import data.model.book.Book;
+import data.model.member.Member;
 import enitiyFactory.EntityFactoryGen;
 
 
@@ -14,6 +16,25 @@ import enitiyFactory.EntityFactoryGen;
 public class LibraryOperations {
 	protected static EntityManager entityManager= EntityFactoryGen.getEntityManager();
 	protected static EntityTransaction transaction =null;
+	/**
+	 * 
+	 * @param member
+	 * @param book
+	 * @return
+	 */
+	
+	public static boolean issueBook(Member member,Book book) {
+		return false;
+		
+	}
+	public static boolean returnBook(Book book) {
+		return false;
+		
+	}
+	public static boolean renewBook(Book book) {
+		return false;
+		
+	}
 	
 	
 	
