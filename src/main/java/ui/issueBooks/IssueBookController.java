108edc20c4368a919d4964ea86eb3831cca351f8
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-08-30 17:30:34 +0300

    modified IssueBookController

--- src/main/java/ui/issueBooks/IssueBookController.java
+++ src/main/java/ui/issueBooks/IssueBookController.java
@@ -1,5 +1,18 @@
 package ui.issueBooks;
 
-public class IssueBookController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class IssueBookController implements Initializable{
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
