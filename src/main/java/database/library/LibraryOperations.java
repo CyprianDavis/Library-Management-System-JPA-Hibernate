9c67ab46095d628cc4e35a5275fd85560924e8d8
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-03 12:40:02 +0300

    Modified issueBook in LibraryOperatins

--- src/main/java/database/library/LibraryOperations.java
+++ src/main/java/database/library/LibraryOperations.java
@@ -5,7 +5,9 @@ import javax.persistence.EntityTransaction;
 
 import data.model.book.Book;
 import data.model.library.IssueBook;
+import data.model.library.Transaction;
 import data.model.member.Member;
+import database.transactions.TransactionsOps;
 import enitiyFactory.EntityFactoryGen;
 
 
@@ -17,6 +19,14 @@ import enitiyFactory.EntityFactoryGen;
 public class LibraryOperations {
 	protected static EntityManager entityManager= EntityFactoryGen.getEntityManager();
 	protected static EntityTransaction transaction =null;
+	
+	
+	private static void createTransaction(String type,Book book,Member member) {
+		Transaction trans = new Transaction(type,book,member);//create transaction object
+		TransactionsOps.insertTransaction(trans);
+		
+		
+	}
 	/**
 	 * 
 	 * @param member
@@ -30,7 +40,8 @@ public class LibraryOperations {
 		transaction = entityManager.getTransaction();
 		transaction.begin();
 		IssueBook issuedBk = new IssueBook(member,book);
-		issuedBk.setDateOfIssuing(issuedOn);
+		issuedBk.setDateOfIssuing(issuedOn);//set date of book check-out
+		createTransaction("Check-Out",book, member);
 		entityManager.persist(issuedBk);
 		String dueDate = issuedBk.getDueDate();
 		transaction.commit();
@@ -46,6 +57,7 @@ public class LibraryOperations {
 		return false;
 		
 	}
+	
 	
 	
 	
