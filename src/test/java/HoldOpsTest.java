884be0f69b390ef07980c780cdb96c820868cefb
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-07 19:45:45 +0300

    added HoldOpsTest

--- src/test/java/HoldOpsTest.java
+++ src/test/java/HoldOpsTest.java
@@ -1,4 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import data.model.book.Book;
+import data.model.library.Hold;
+import data.model.member.Member;
+import database.catalog.Catalog;
+import database.memberOperations.MembersOperations;
 
 public class HoldOpsTest {
+	Member member =null;
+	Book book=null;
+	Hold hold =null;
+	@BeforeEach
+	void init() {
+		member = MembersOperations.findMember(null);
+		book = Catalog.findBook(null);
+		 hold = new Hold(member,book,3);
+	}
 
 }
