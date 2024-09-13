c38604d73133b5ba899b74d008c638efb422a960
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-13 12:26:40 +0300

    added searchMember method

--- src/main/java/ui/issueBooks/IssueBookController.java
+++ src/main/java/ui/issueBooks/IssueBookController.java
@@ -7,11 +7,16 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
 import data.model.book.Book;
+import data.model.member.Member;
+import database.memberOperations.MembersOperations;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class IssueBookController implements Initializable{
 	@FXML
@@ -32,7 +37,7 @@ public class IssueBookController implements Initializable{
 	private TableColumn<Book,String>author;
 	@FXML
 	private TableColumn<Book,String>category;
-	
+	private Member member=null;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -48,7 +53,29 @@ public class IssueBookController implements Initializable{
 		author.setCellValueFactory(new PropertyValueFactory<>("author"));
 		category.setCellValueFactory(new PropertyValueFactory<>("category"));
 		
+	}
+	@FXML
+	private void searchMember() {
+		if(memberId.getText().isEmpty()) {
+			showAlert(Alert.AlertType.ERROR, ((Stage) book.getScene().getWindow()), "Issue Book", "Please enter Member ID");
+			 return;
+		}
+		 member = MembersOperations.findMember(memberId.getText());//check if member exists
+		 if(member==null) {
+			 showAlert(Alert.AlertType.ERROR, ((Stage) book.getScene().getWindow()), "Issue Book", "Please enter Vaild Member ID");
+			 return;
+		 }
+			 
 		
 	}
+	//Handles Alert Messages
+	private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+		Alert alert = new Alert(alertType);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.initOwner(owner);
+		alert.show();
+		}
 
 }
