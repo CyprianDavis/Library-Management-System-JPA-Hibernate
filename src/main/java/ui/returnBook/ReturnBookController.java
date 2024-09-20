c1c445a8c52c7a1d1cbb32030c51aee231e90f3e
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-20 15:04:00 +0300

    modified retunBookController added fields

--- src/main/java/ui/returnBook/ReturnBookController.java
+++ src/main/java/ui/returnBook/ReturnBookController.java
@@ -3,10 +3,18 @@ package ui.returnBook;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class ReturnBookController implements Initializable{
-
+	@FXML
+	private JFXButton returnBtn;
+	@FXML
+	private JFXTextField book;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
