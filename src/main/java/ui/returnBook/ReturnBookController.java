2ed8fde0fc9105ea46c1c54e7ea17107e9bef76a
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-20 15:31:27 +0300

    modified ReturnBookController

--- src/main/java/ui/returnBook/ReturnBookController.java
+++ src/main/java/ui/returnBook/ReturnBookController.java
@@ -6,9 +6,11 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
+import database.catalog.Catalog;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class ReturnBookController implements Initializable{
@@ -24,7 +26,18 @@ public class ReturnBookController implements Initializable{
 	}
 	@FXML
 	private void returnBook() {
-		
+		if(book.getText().isEmpty()) {
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book!", "Please Enter Book Number ");
+			return;
+		}
+		if(!Catalog.bookExists(book.getText())) {
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Issue Book!", "UNKONWN BOOK ID ");
+			return;
+		}
+		if(!Catalog.isBookCheckedout(book.getText())) {
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Issue Book!", "Book is not checked out ");
+			return;
+		}
 	}
 	//Handles Alert Messages
 	private  void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
