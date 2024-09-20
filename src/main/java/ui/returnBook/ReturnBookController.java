879b9a51bbfc30d2306d195ff80ee157e322d837
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-20 16:41:07 +0300

    modified ReturnBookController

--- src/main/java/ui/returnBook/ReturnBookController.java
+++ src/main/java/ui/returnBook/ReturnBookController.java
@@ -6,7 +6,11 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
+import data.model.book.Book;
+import data.model.member.Member;
 import database.catalog.Catalog;
+import database.library.LibraryOperations;
+import database.memberOperations.MembersOperations;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -18,7 +22,7 @@ public class ReturnBookController implements Initializable{
 	@FXML
 	private JFXButton returnBtn;
 	@FXML
-	private JFXButton member;
+	private JFXTextField memberId;
 	@FXML
 	private JFXTextField book;
 	
@@ -29,19 +33,43 @@ public class ReturnBookController implements Initializable{
 	}
 	@FXML
 	private void returnBook() {
+		if(memberId.getText().isEmpty()) {
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book", "Please Enter Member ID Number ");
+			return;
+		}
+		if(!MembersOperations.memberExists(memberId.getText())) {
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book", "INVALID Member ID ");
+			return;
+		}
 		if(book.getText().isEmpty()) {
-			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book!", "Please Enter Book Number ");
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book", "Please Enter Book Number ");
 			return;
 		}
 		if(!Catalog.bookExists(book.getText())) {
-			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Issue Book!", "UNKONWN BOOK ID ");
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Retrun Book", "UNKONWN BOOK ID ");
 			return;
 		}
 		if(!Catalog.isBookCheckedout(book.getText())) {
-			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Issue Book!", "Book is not checked out ");
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Return Book", "Book is not checked out ");
 			return;
 		}
-		//return issued book
+		//Extract member entity from database
+		Member member = MembersOperations.findMember(memberId.getText());
+		//Extract Book entity from database
+		Book bk = Catalog.findBook(book.getText());
+		
+		//return book 
+		if(LibraryOperations.returnBook(bk, member) >0) {
+			showAlert(Alert.AlertType.INFORMATION, ((Stage) book.getScene().getWindow()), "Return Book", "Operation Successfull ");
+			return;
+		}
+		else{
+			showAlert(Alert.AlertType.WARNING, ((Stage) book.getScene().getWindow()), "Retrun Book", "Operation Failed ");
+			return;
+			
+		}
+			
+		
 		
 	}
 	//Handles Alert Messages
