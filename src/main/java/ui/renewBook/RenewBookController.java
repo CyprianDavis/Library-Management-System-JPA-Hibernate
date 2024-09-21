8a23cb5143086583e368f400482d3386b39e3ff0
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-21 17:38:42 +0300

    added fields in RenewBookController

--- src/main/java/ui/renewBook/RenewBookController.java
+++ src/main/java/ui/renewBook/RenewBookController.java
@@ -3,10 +3,19 @@ package ui.renewBook;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class RenewBookController implements Initializable {
-
+	@FXML
+	private JFXTextField memberId;
+	@FXML
+	private JFXTextField book;
+	@FXML
+	private JFXButton renewBtn;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
