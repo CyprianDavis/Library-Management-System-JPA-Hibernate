96df14e95cb22c63d07bd3ffb465fcccc0c02683
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-23 03:21:28 +0300

    modified PlaceHoldController added feilds and method

--- src/main/java/ui/hold/PlaceHoldController.java
+++ src/main/java/ui/hold/PlaceHoldController.java
@@ -3,14 +3,41 @@ package ui.hold;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
 
 public class PlaceHoldController  implements Initializable{
+	@FXML
+	private JFXTextField memberId;
+	@FXML
+	private JFXTextField bookId;
+	@FXML
+	private JFXButton placeHold;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@FXML
+	private void placeHold() {
+		
+	}
+	//Handles Alert Messages
+	private  void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+		Alert alert = new Alert(alertType);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.initOwner(owner);
+		alert.show();
+	}
+	
 }
+
+
