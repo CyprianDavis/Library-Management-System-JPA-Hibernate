9cfb113154fd31437359939bb1bb412bce674d8c
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-26 22:07:25 +0300

    modified ViewHoldsOnBookController added feilds

--- src/main/java/ui/hold/ViewHoldsOnBookController.java
+++ src/main/java/ui/hold/ViewHoldsOnBookController.java
@@ -3,9 +3,33 @@ package ui.hold;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class ViewHoldsOnBookController implements Initializable{
+	@FXML
+	private JFXTextField searchBk;
+	@FXML
+	private TableView<DisplayHoldsDetails> holdsTable;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>holdId;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>member;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>book;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>reservationDate;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>status;
+	@FXML
+	private TableColumn<DisplayHoldsDetails,String>comment;
+	
+	
+
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
