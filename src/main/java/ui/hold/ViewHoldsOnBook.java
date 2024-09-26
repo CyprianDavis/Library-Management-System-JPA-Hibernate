811854a6e5d777d5f079cae8fe3eba3482aea90d
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2024-09-26 18:26:15 +0300

    created ViewHoldsOnBook.java

---
+++ src/main/java/ui/hold/ViewHoldsOnBook.java
@@ -0,0 +1,16 @@
+package ui.hold;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ViewHoldsOnBook implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
